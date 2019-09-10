05782d7f6ec2c8632d98357b336644e524edbe0c
Joi <khrissianjoi@gmail.com>
2019-09-10 16:41:03 +0100

    environment

--- EnvironmentalIssues.js
+++ EnvironmentalIssues.js
@@ -13,7 +13,15 @@ fetch(url).then(response =>
 
 function makeGraphs(data) {
     data = dataConversion(data)
-    AsylumDescisionPerYearBarChart(data)
+    data.forEach(function(d){
+        if(typeof d['count'] === 'string') {
+            
+            d['count'] = parseInt(d['count'])
+        }
+        console.log(d['count'])
+      });
+    EnvironmentalIssuesPerYearBarChart(data)
+    EnvironmentalIssuesPerYearLineChart(data)
     dc.renderAll();
 }
 
@@ -37,21 +45,89 @@ function dataConversion(data) {
     return cleanData
 }
 
-function AsylumDescisionPerYearBarChart(CountryData) {
+
+function EnvironmentalIssuesPerYearLineChart(data) {
+    var ndx = crossfilter(data)
+    var dateDim = ndx.dimension(dc.pluck("year"));
+    var sentiGroup = dateDim.group().reduce(
+        function(p, v) {
+            //average calculator
+            p.count++;
+            p.total += v['count'];
+            p.average = p.total / p.count;
+            return p;
+        },
+        function(p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.total = 0;
+                p.average = 0;
+            } else {
+                p.total -= v['count'];
+                p.average = p.total / p.count;
+            }
+            return p;
+        },
+        function () {
+            return { count: 0, total: 0, average: 0};
+        }
+    )
+    console.log(sentiGroup.all())
+    var chart = dc.lineChart("#EnvironmentalIssuesLine")
+        .width(1800)
+        .height(500)
+        .margins({top: 10, right: 50, bottom: 50, left: 50})
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .brushOn(false)
+        .xAxisLabel('Year')
+        .yAxisLabel('Housing Deprivation')
+        .dimension(dateDim)
+        .group(sentiGroup)
+        .valueAccessor(function(d) {
+            return d.value.average
+        })
+}
+function EnvironmentalIssuesPerYearBarChart(CountryData) {
     console.log(CountryData)
     var ndx = crossfilter(CountryData)
     var countryDim = ndx.dimension(dc.pluck("year"));
-    var countryMix = countryDim.group().reduceSum(dc.pluck("count"))
+    var countryMix = countryDim.group().reduce(
+        function(p, v) {
+            //average calculator
+            p.count++;
+            p.total += v['count'];
+            p.average = p.total / p.count;
+            return p;
+        },
+        function(p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.total = 0;
+                p.average = 0;
+            } else {
+                p.total -= v['count'];
+                p.average = p.total / p.count;
+            }
+            return p;
+        },
+        function () {
+            return { count: 0, total: 0, average: 0};
+        }
+    )
     dc.barChart("#EnvironmentalIssues")
         .width(1550)
         .height(550)
         .margins({top: 20, right: 50, bottom: 100, left: 80})
         .dimension(countryDim)
         .group(countryMix)
+        .valueAccessor(function(d) {
+            return d.value.average
+        })
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .xAxisLabel("Year")
-        .yAxisLabel("Percentage of Environmental Issues per household")
+        .yAxisLabel("Percentage of Housing Deprivation")
         .yAxis().ticks(20);
 }
